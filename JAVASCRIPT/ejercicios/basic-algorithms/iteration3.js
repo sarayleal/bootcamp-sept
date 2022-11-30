b9610e1f2cbf63a576134806e9fd592345d14014
saray leal <sarayleal2022@gmail.com>
2022-11-30 11:05:35 +0100

    basic-algorithms

---
+++ JAVASCRIPT/ejercicios/basic-algorithms/iteration3.js
@@ -0,0 +1,39 @@
+
+/*Iteración #3: Operadores
+1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+
+1.2 Divide 10 por 2 y muestra el resultado en un console.
+
+1.3 Muestra mediante un console el resto de dividir 15 por 9.
+
+1.4 Usa el correcto operador de asignación que resultará en o = 15, 
+teniendo dos variables p = 10 y j = 5.
+
+1.5 Usa el correcto operador de asignación que resultará en i = 50,
+teniendo dos variables c = 10 y m = 5.*/
+
+1.1
+let number1 = 10;
+let number2 = 5;
+console.log(10*5)
+
+
+1.2
+let a = 10;
+let b = 2;
+console.log(10 / 2);
+
+1.3
+console.log(15/9);
+
+1.4
+let p = 10;
+let j = 5
+let O = (p + j);
+console.log(O);
+
+1.5
+let c = 10
+let m = 5
+let i = (c * m);
+console.log(i);
