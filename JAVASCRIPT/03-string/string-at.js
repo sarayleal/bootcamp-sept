8b7a2cbb09f22f219c6a64163a56f0140b1c03b2
saray leal <sarayleal2022@gmail.com>
2022-12-04 21:43:34 +0100

    String-at

---
+++ JAVASCRIPT/03-string/string-at.js
@@ -0,0 +1,9 @@
+//El metodo .at, te devuelve el elemento en la posicion indicada.
+
+const sentence = "Mi nombre es Saray Leal";
+
+let index = 11;
+
+console.log(`Voy a usar el index ${index} para que me devuelva el elemento que se encuentra en la posicion del ${sentence.at(index)}`);
+
+console.log(`Voy a usar el index ${index} para que me devuelva el elemento que se encuentra en la posicion del ${sentence.at(index -3)}`);
