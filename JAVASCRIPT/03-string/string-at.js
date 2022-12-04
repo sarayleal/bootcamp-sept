dddc4521823d5fac2ee2534f8ae07027b24c3ca6
saray leal <sarayleal2022@gmail.com>
2022-12-04 22:18:25 +0100

    string at

--- JAVASCRIPT/03-string/string-at.js
+++ JAVASCRIPT/03-string/string-at.js
@@ -7,3 +7,14 @@ let index = 11;
 console.log(`Voy a usar el index ${index} para que me devuelva el elemento que se encuentra en la posicion del ${sentence.at(index)}`);
 
 console.log(`Voy a usar el index ${index} para que me devuelva el elemento que se encuentra en la posicion del ${sentence.at(index -3)}`);
+
+
+
+const sentence1 = "Una frase para que?";
+
+let index1 = 18;
+
+console.log(`Voy a usar el index ${index1} para que me devuelva el elemento que se encuentra en esa posición  ${sentence1.at(index1)}`);
+ 
+//Cuando a at, no le pasamos valor, por defecto es 0. si le pasamos -1, por ejemplo, será el ultimo elemento y así sucesivamente.
+console.log(`Voy a no usar el index para que me devuelva la posicion por defecto  ${sentence1.at()}`);
