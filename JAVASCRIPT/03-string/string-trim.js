10a24e8262e884a5f438014ed99acb3492a2c21c
saray leal <sarayleal2022@gmail.com>
2022-12-09 20:33:55 +0100

    teoría

---
+++ JAVASCRIPT/03-string/string-trim.js
@@ -0,0 +1,10 @@
+const greetin = "    Hello world       ";
+console.log(greetin.trim());
+
+
+const greetin2 = "   Hello world          .";
+console.log(greetin2.trimStart());
+
+
+const greetin3 = "Hello world         .";
+console.log(greetin3.trimEnd());
---
+++ JAVASCRIPT/03-string/string-valueof.js
@@ -0,0 +1,6 @@
+const stringObject = new String ('foo');
+console.log(stringObject);
+
+const sentence =  new String ('Saray Morena');
+console.log(sentence);
+
