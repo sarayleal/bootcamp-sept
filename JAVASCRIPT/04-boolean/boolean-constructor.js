7c4115319171891ed81765f531c2c2a34f3fdf34
saray leal <sarayleal2022@gmail.com>
2022-12-10 15:45:43 +0100

    teoría

---
+++ JAVASCRIPT/04-boolean/boolean-constructor.js
@@ -0,0 +1,12 @@
+const flag = new Boolean();
+console.log(flag);
+
+const flagOther = new Boolean(5);
+console.log(flagOther);
+
+const flagOther2 = new Boolean(false);
+console.log(flagOther2);
+
+const flagOther3 = new Boolean(undefined);
+console.log(flagOther3);
+
---
+++ JAVASCRIPT/04-boolean/boolean-valueof.js
@@ -0,0 +1,11 @@
+const x = new Boolean();
+console.log(x.valueOf());
+
+const y = new Boolean("Saray");
+console.log(y.valueOf());
+
+const r = new Boolean(10);
+console.log(r.valueOf)
+
+const m = new Boolean("");
+console.log(m.valueOf());
