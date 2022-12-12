715bcc277c3855ee00c041f0c8c61b4bb130acb2
saray leal <sarayleal2022@gmail.com>
2022-12-12 20:48:33 +0100

    operators

---
+++ JAVASCRIPT/07-operators/operators-code.js
@@ -0,0 +1,35 @@
+//!operador de asignación
+let number = 5
+
+
+//!operador de adiccion
+number +=1
+
+
+//!operador de sustraccion
+number -= 2
+
+
+//!operador de multiplicacion
+number *= 6
+
+
+//!operador de division
+number /=7
+
+
+//!operqador de modulo
+number %=3
+
+
+//!operador igualdad
+function operadorIgualdad(a,b) {
+    if(a===b) {console.log('Bienn, has acertado')}
+}
+operadorIgualdad(2,2);
+
+
+
+
+
+
