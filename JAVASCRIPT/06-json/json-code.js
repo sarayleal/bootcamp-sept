e44d6f34ef476e0c6aab601cd86be814599980ef
saray leal <sarayleal2022@gmail.com>
2022-12-12 12:41:52 +0100

    teoría

---
+++ JAVASCRIPT/06-json/json-code.js
@@ -0,0 +1,14 @@
+const myUser = {name :'Saray', age:40};
+console.log(myUser);
+console.log(JSON.stringify(myUser));
+
+
+const myBoyFriend = {name:'David',age:36};
+console.log(JSON.stringify(myBoyFriend));
+
+
+const myHome = {adress:'Calle la Aldea', number:60}
+console.log(myHome);
+console.log(JSON.stringify(myHome));
+
+
