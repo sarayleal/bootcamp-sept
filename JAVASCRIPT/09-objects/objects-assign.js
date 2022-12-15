803038bd606f4789cebbe3538f07efaa9d330bb0
saray leal <sarayleal2022@gmail.com>
2022-12-15 16:53:05 +0100

    objects-assign

---
+++ JAVASCRIPT/09-objects/objects-assign.js
@@ -0,0 +1,19 @@
+const target = {a:1, b:2};
+const source = {b:3, c:4};
+
+Object.assign(target, source);
+
+console.log('target', target);
+console.log('soource', source);
+
+
+
+const person = {name:'Saray', age:40};
+const person2 = {name:'David', age:36};
+
+Object.assign(person, person2);
+
+console.log('person', person);
+console.log('person2', person2);
+
+
