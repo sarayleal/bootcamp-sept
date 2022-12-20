f6cc1791ab56ee04a3cb07628a106ed1f328c8a8
saray leal <sarayleal2022@gmail.com>
2022-12-20 16:38:56 +0100

    arrays

---
+++ JAVASCRIPT/10-arrays/app.js
@@ -0,0 +1,31 @@
+//! .at      nos devuelve la posicion de un valor dentro de un array.
+const array1 = [1,5,7,9,12];
+const index1 = 4;
+console.log(array1.at(index1));
+
+
+const arrayexample = ["saray", "asier", "india", "david"];
+const index2 = 5;
+console.log(arrayexample.at(index2));
+
+
+
+//! .concat    nos devuelve un array concatenado, de dos o varios arrays
+const arrayConcat1 = [1,2,3];
+const arrayConcat2 = ['Saray', 'David', 'India'];
+const arrayConcat3 = arrayConcat1.concat(arrayConcat2);
+const arrayConcat4 = [{a:'Tyson', b: 'Michi'}];
+const arrayConcatSpread = [...arrayConcat1, ...arrayConcat2, ...arrayConcat4];
+console.log(arrayConcat3);
+console.log(arrayConcatSpread);
+
+
+
+
+
+
+
+
+
+
+
